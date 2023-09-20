46b659bf40bb6f7ea58c2a5e41499e4b25b5056b
alexc <alex.codinab@gmail.com>
2023-09-20 18:54:11 +0200

    Block camera to not get inside the roof

--- Scripts/Camera.cs
+++ Scripts/Camera.cs
@@ -1,17 +1,28 @@
+using System;
 using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    private const float MaxHeight = 13.5f;
+    
+    public GameObject player;
 
+    private void Start()    
+    {
+        if (player == null)
+        {
+            Debug.LogError("Player not found");
+        }
+    }
 
     void Update()
     {
-        //Follow the player
-        GameObject player = GameObject.Find("Player");
+        // Follow the player
         if (player == null) return;
-        
-        Vector2 playerPosition = player.transform.position;
 
-        transform.position = new Vector3(playerPosition.x, playerPosition.y + 2f, transform.position.z);
+        Vector2 playerPosition = player.transform.position;
+        
+        float newYPosition = Mathf.Min(playerPosition.y + 2f, MaxHeight);
+        transform.position = new Vector3(playerPosition.x, newYPosition, transform.position.z);
     }
 }
